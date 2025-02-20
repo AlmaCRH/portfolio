7c7d1586b86399b001f9d47748f1ece997577464
AlmaCRH <alma5crh@gmail.com>
2025-02-20 06:13:49 +0000

    deploy configuration installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   plugins: [tailwindcss()],
@@ -8,4 +10,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
