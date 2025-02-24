b73acd27bb9603ebd88f3b74061fe1868623549f
AlmaCRH <alma5crh@gmail.com>
2025-02-24 07:22:47 +0000

    GENERAL - Cards added - Work images optimized - Little refactor (for typescript warnings) added - Added the logic to hide a card clicking outside the card

---
+++ src/components/jobsExperience.js
@@ -0,0 +1,40 @@
+const jobsExperience = [
+  {
+    title: "Edukami.ai",
+    img: "/workImages/edukami.webp",
+    text: "Educational platform powered with AI that helps enterprises to optimize the learning experience of their employees. Made with Vue, TailwindCSS, Node.js, MongoDB, Express, openAI and FireBase.",
+    link: "https://edukami.ai/",
+  },
+  {
+    title: "Reboot Talent",
+    img: "/workImages/talent.webp",
+    text: "Internal project that accelerates the process of talent acquisition and onboarding. Made with Vue, TailwindCSS, Node.js, MongoDB and Express.",
+    link: "https://reboot.academy/talent",
+  },
+  {
+    title: "WISE-C",
+    img: "/workImages/wise.webp",
+    text: "Web application for a NGO that supports STEAM womens in the Canary Islands. Givin them visibility and empowerment. Made with React, CSS, Email.js and Stripe.",
+    link: "https://wisecanarias.com/",
+  },
+  {
+    title: "AfricaNdoto Frontend",
+    img: "/workImages/africandotoFront.webp",
+    text: "Frontend application to solve the problem of the NGO to get more volunteers and donors and gain fiability through transparency. Made with React, CSS and Material UI.",
+    link: "https://reboot.academy/talent",
+  },
+  {
+    title: "AfricaNdoto Backend",
+    img: "/workImages/africandotoBack.webp",
+    text: "Backend application to help this NGO with their problems of managing the people associated, the donations and their projects. Made with Node.js, Express and MySQL.",
+    link: "https://reboot.academy/talent",
+  },
+  {
+    title: "Demons Defender",
+    img: "/workImages/demonsDefender.webp",
+    text: "Web game based on the tower defense genre, made with pure HTML, CSS and JavaScript.",
+    link: "https://reboot.academy/talent",
+  },
+];
+
+export default jobsExperience;
