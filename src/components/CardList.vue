007b53052e32278c1620d39e98a3b8d0afe3b833
AlmaCRH <alma5crh@gmail.com>
2025-03-03 16:40:56 +0000

    Translated application to Vue3

---
+++ src/components/CardList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="text-center mt-5" v-for="(job, index) in jobsExperience">
+    <span
+      :key="index"
+      :data-id="job.title"
+      class="text-white font-bebas text-7xl cursor-pointer hover:text-[#fffa9d] active:text-[#fffa9d]"
+      @click.stop="showCard(job.title)"
+    >
+      {{ job.title }}
+    </span>
+  </div>
+
+  <Card
+    v-for="(job, index) in jobsExperience"
+    :id="job.id"
+    :key="job.title"
+    :title="job.title"
+    :text="job.text"
+    :img="job.img"
+    :link="job.link"
+    :isVisible="visibleCardId === job.title"
+    @close="closeCard"
+  />
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+import jobsExperience from "./jobsExperience.js";
+import { ref, onMounted, onUnmounted } from "vue";
+
+const visibleCardId = ref(null);
+
+const showCard = (id) => {
+  visibleCardId.value = id;
+};
+
+const closeCard = () => {
+  visibleCardId.value = null;
+};
+
+const handleClickOutside = (e) => {
+  const card = document.querySelector(".work-card:not(.hidden)");
+  if (card && !card.contains(e.target)) {
+    closeCard();
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("click", handleClickOutside);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("click", handleClickOutside);
+});
+</script>
